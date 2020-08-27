9dbe8bc471fa64eb4d351bb8bee69a0064be3fd3
Zahid Zafar <zahidzafar51@gmail.com>
2020-08-27 15:38:06 +0500

    Compile Issues Resolved and Example Folder Added (#31)
    * compile issues
    
    * Scrip and Scene folder deleted
    
    * Logs added and Code revert
    
    * Folder hierarchy changes

--- Assets/Plugins/Notifications/Impls/ProxyNotificationsService.cs
+++ Assets/Plugins/Notifications/Impls/ProxyNotificationsService.cs
@@ -9,15 +9,17 @@ namespace Notifications.Impls
 
 		public ProxyNotificationsService(Action<IEnumerator> startCoroutine)
 		{
+
 #if UNITY_EDITOR
-			_service = new EditorNotificationsService();
+            _service = new EditorNotificationsService();
 #elif UNITY_ANDROID
-			_service = new Notifications.Impls.Android.AndroidNotificationsService();
+            _service = new Notifications.Impls.Android.AndroidNotificationsService();
 #elif UNITY_IOS
 			_service = new Notifications.Impls.iOs.IOsNotificationsService(startCoroutine);
-#endif
-		}
 
-		public void GetToken(Action<string> result) => _service.GetToken(result);
+#endif
+        }
+
+        public void GetToken(Action<string> result) => _service.GetToken(result);
 	}
 }
--- Assets/Plugins/Notifications/Impls/iOs/iOsNotificationsService.cs
+++ Assets/Plugins/Notifications/Impls/iOs/iOsNotificationsService.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
+#if UNITY_IOS
 using Unity.Notifications.iOS;
+#endif
 using UnityEngine;
 
 namespace Notifications.Impls.iOs
@@ -22,6 +24,7 @@ namespace Notifications.Impls.iOs
         private IEnumerator RequestAuthorization(Action<string> result)
         {
             Debug.Log("[IOsNotificationsService] RequestAuthorization");
+#if UNITY_IOS
             using (var req = new AuthorizationRequest(AuthorizationOption.Alert | AuthorizationOption.Badge, true))
             {
                 while (!req.IsFinished)
@@ -38,6 +41,10 @@ namespace Notifications.Impls.iOs
                 
                 result.Invoke(req.DeviceToken);
             }
+#else
+            Debug.Log("[Countly] IOsNotificationsService, RequestAuthorization, execution will be skipped, Unity.Notification.iOS exists only on IOS platform");
+            yield return null;
+#endif
         }
     }
 }
