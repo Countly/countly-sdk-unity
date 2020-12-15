b2c4b8b3e529d0e5a4ada6324ad2291d101c67c6
Zahid Zafar <>
2020-12-15 16:47:27 +0500

    Check added

--- Assets/Plugins/CountlySDK/Persistance/Dao/EventNumberInSameSessionDao.cs
+++ Assets/Plugins/CountlySDK/Persistance/Dao/EventNumberInSameSessionDao.cs
@@ -4,6 +4,7 @@ using iBoxDB.LocalServer;
 using Plugins.CountlySDK.Models;
 using Plugins.CountlySDK.Persistance.Entities;
 using Plugins.iBoxDB;
+using UnityEngine;
 
 namespace Plugins.CountlySDK.Persistance.Dao
 {
@@ -21,19 +22,28 @@ namespace Plugins.CountlySDK.Persistance.Dao
             
             var ql = _stringBuilder.Append("from ").Append(Table)
                 .Append(" where EventKey==?").ToString();
-            
-            var entities =  Auto.Select<EventNumberInSameSessionEntity>(ql, eventKey);
-            
-            if (entities.Count > 1)
-            {
-                throw new ArgumentException("Only one or zero event can be assigned to entity with key " + eventKey + ". " 
-                                            + entities.Count + " events found.");
-            }
-            
-            if (entities.Count == 0)
-                return null;
 
-            return entities[0];
+            try
+            {
+
+                var entities = Auto.Select<EventNumberInSameSessionEntity>(ql, eventKey);
+
+                if (entities.Count > 1)
+                {
+                    throw new ArgumentException("Only one or zero event can be assigned to entity with key " + eventKey + ". "
+                                                + entities.Count + " events found.");
+                }
+
+                if (entities.Count == 0)
+                    return null;
+
+                return entities[0];
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("[Countly] EventNumberInSameSessionEntity GetByEventName: Couldn't complete db operation, [" + ex.Message + "]");
+                return null;
+            }
         }
     }
 }
