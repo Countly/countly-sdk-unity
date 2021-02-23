3ec43e6f47588bad40ab2814d8a9ca0e6dde2972
Zahid Zafar <>
2021-02-23 17:41:20 +0500

    sdk init logs added

--- Assets/Plugins/CountlySDK/Countly.cs
+++ Assets/Plugins/CountlySDK/Countly.cs
@@ -156,6 +156,7 @@ namespace Plugins.CountlySDK
         public void Init(CountlyConfiguration configuration)
         {
             if (IsSDKInitialized) {
+                _logHelper.Error("SDK has already been initialized!");
                 return;
             }
 
@@ -296,6 +297,7 @@ namespace Plugins.CountlySDK
         internal void ClearStorage()
         {
             if (!IsSDKInitialized) {
+                _logHelper.Error("SDK must be initialised before calling ClearStorage");
                 return;
             }
             _logHelper.Debug("[Countly] ClearStorage");
