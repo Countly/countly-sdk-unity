291ff5f1917a5e4c01f1a5a62d0a4e82326d511c
Zahid Zafar <zahidzafar51@gmail.com>
2021-04-21 23:14:10 +0500

    Event Service unit tests added. (#85)
    Co-authored-by: Zahid Zafar <>

--- Assets/Tests/PlayModeTests/EventTests.cs
+++ Assets/Tests/PlayModeTests/EventTests.cs
@@ -15,9 +15,153 @@ namespace Tests
         private readonly string _serverUrl = "https://xyz.com/";
         private readonly string _appKey = "772c091355076ead703f987fee94490";
 
+        /// <summary>
+        /// It validates the event repository initial state.
+        /// </summary>
+        [Test]
+        public void TestEventRepoInitialState()
+        {
+            CountlyConfiguration configuration = new CountlyConfiguration {
+                ServerUrl = _serverUrl,
+                AppKey = _appKey,
+            };
+
+            Countly.Instance.Init(configuration);
+
+            Assert.AreNotEqual(null, Countly.Instance.Events);
+            Assert.AreEqual(0, Countly.Instance.Events._viewEventRepo.Count);
+            Assert.AreEqual(0, Countly.Instance.Events._nonViewEventRepo.Count);
+
+        }
+
+        /// <summary>
+        /// It checks the working of event service if no event consent is given.
+        /// </summary>
+        [Test]
+        public async void TestEventConsent()
+        {
+            CountlyConfiguration configuration = new CountlyConfiguration {
+                ServerUrl = _serverUrl,
+                AppKey = _appKey,
+                RequiresConsent = true
+            };
+
+            Countly.Instance.Init(configuration);
+
+            Assert.AreNotEqual(null, Countly.Instance.Events);
+            Assert.AreEqual(0, Countly.Instance.Events._viewEventRepo.Count);
+            Assert.AreEqual(0, Countly.Instance.Events._nonViewEventRepo.Count);
+
+            await Countly.Instance.Events.RecordEventAsync("test_event");
+            Assert.AreEqual(0, Countly.Instance.Events._nonViewEventRepo.Count);
+
+            await Countly.Instance.Events.ReportCustomEventAsync("test_event", segmentation: null, sum: 23, duration: 5);
+            Assert.AreEqual(0, Countly.Instance.Events._nonViewEventRepo.Count);
+
+
+        }
+
+        /// <summary>
+        /// It validates functionality of method 'RecordEventAsync'.
+        /// </summary>
+        [Test]
+        public async void TestEventMethod_RecordEventAsync()
+        {
+            CountlyConfiguration configuration = new CountlyConfiguration {
+                ServerUrl = _serverUrl,
+                AppKey = _appKey,
+            };
+
+            Countly.Instance.Init(configuration);
+
+            Assert.AreNotEqual(null, Countly.Instance.Events);
+            Assert.AreEqual(0, Countly.Instance.Events._viewEventRepo.Count);
+            Assert.AreEqual(0, Countly.Instance.Events._nonViewEventRepo.Count);
+
+            await Countly.Instance.Events.RecordEventAsync("test_event");
+            Assert.AreEqual(1, Countly.Instance.Events._nonViewEventRepo.Count);
+
+            CountlyEventModel model = Countly.Instance.Events._nonViewEventRepo.Dequeue();
+
+            Assert.AreEqual("test_event", model.Key);
+            Assert.AreEqual(0, model.Sum);
+            Assert.AreEqual(1, model.Count);
+            Assert.AreEqual(null, model.Duration);
+            Assert.AreEqual(null, model.Segmentation);
+        }
+
+        /// <summary>
+        /// It validates the working of event service if 'EventQueueThreshold' limit reach.
+        /// </summary>
+        [Test]
+        public async void TestEvent_EventQueueThreshold_Limit()
+        {
+            CountlyConfiguration configuration = new CountlyConfiguration {
+                ServerUrl = _serverUrl,
+                AppKey = _appKey,
+                EventQueueThreshold = 3
+            };
+
+            Countly.Instance.Init(configuration);
+
+            Assert.AreNotEqual(null, Countly.Instance.Events);
+            Assert.AreEqual(0, Countly.Instance.Events._viewEventRepo.Count);
+            Assert.AreEqual(0, Countly.Instance.Events._nonViewEventRepo.Count);
+
+            await Countly.Instance.Events.RecordEventAsync("test_event_1");
+            Assert.AreEqual(1, Countly.Instance.Events._nonViewEventRepo.Count);
+
+            await Countly.Instance.Events.RecordEventAsync("test_event_2");
+            Assert.AreEqual(2, Countly.Instance.Events._nonViewEventRepo.Count);
+
+            await Countly.Instance.Events.RecordEventAsync("test_event_3");
+            Assert.AreEqual(0, Countly.Instance.Events._nonViewEventRepo.Count);
+
+
+        }
+
+        /// <summary>
+        /// It validates functionality of method 'ReportCustomEventAsync'.
+        /// </summary>
+        [Test]
+        public async void TestEventMethod_ReportCustomEventAsync()
+        {
+            CountlyConfiguration configuration = new CountlyConfiguration {
+                ServerUrl = _serverUrl,
+                AppKey = _appKey,
+            };
+
+            Countly.Instance.Init(configuration);
+
+            Assert.AreNotEqual(null, Countly.Instance.Events);
+            Assert.AreEqual(0, Countly.Instance.Events._viewEventRepo.Count);
+            Assert.AreEqual(0, Countly.Instance.Events._nonViewEventRepo.Count);
+
+
+            Dictionary<string, object> segments = new Dictionary<string, object>{
+            { "key1", "value1"},
+            { "key2", "value2"}
+            };
+
+            SegmentModel segmentModel = new SegmentModel(segments);
+
+            await Countly.Instance.Events.RecordEventAsync("test_event", segmentation: segmentModel, sum: 23, duration: 5);
+
+            CountlyEventModel model = Countly.Instance.Events._nonViewEventRepo.Dequeue();
+
+            Assert.AreEqual("test_event", model.Key);
+            Assert.AreEqual(23, model.Sum);
+            Assert.AreEqual(1, model.Count);
+            Assert.AreEqual(5, model.Duration);
+            Assert.AreEqual(2, model.Segmentation.Count);
+            Assert.AreEqual("value1", model.Segmentation["key1"]);
+            Assert.AreEqual("value2", model.Segmentation["key2"]);
+        }
+
         [TearDown]
         public void End()
         {
+            Countly.Instance.ClearStorage();
             Object.DestroyImmediate(Countly.Instance);
         }
     }
