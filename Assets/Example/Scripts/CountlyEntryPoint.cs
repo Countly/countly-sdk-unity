c3d37f32cce98fe99fbf283544da910dba7fa57b
Zahid Zafar <>
2021-10-25 15:54:22 +0500

    test commit

--- Assets/Example/Scripts/CountlyEntryPoint.cs
+++ Assets/Example/Scripts/CountlyEntryPoint.cs
@@ -17,8 +17,8 @@ public class CountlyEntryPoint : MonoBehaviour, INotificationListener
     private void Awake()
     {
         CountlyConfiguration configuration = new CountlyConfiguration {
-            ServerUrl = "https://try.count.ly/",
-            AppKey = "YOUR_APP_KEY",
+            ServerUrl = "https://master.count.ly/",
+            AppKey = "8c1d653f8f474be24958b282d5e9b4c4209ee552",
             EnableConsoleLogging = true,
             Salt = "test-salt-checksum",
             EnablePost = true,
