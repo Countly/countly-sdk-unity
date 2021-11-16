f899eebaca20bf415e7abffdf2f3fd25d3bf260e
ZahidZafar <zahidzafar51@gmail.com>
2021-11-16 17:17:23 +0500

    Migration test updated

--- Assets/Tests/PlayModeTests/MigrationTests.cs
+++ Assets/Tests/PlayModeTests/MigrationTests.cs
@@ -10,6 +10,10 @@ using Newtonsoft.Json.Linq;
 using Plugins.CountlySDK.Helpers;
 using UnityEditor.PackageManager.Requests;
 using System.Linq;
+using iBoxDB.LocalServer;
+using Plugins.CountlySDK.Persistance.Entities;
+using Plugins.CountlySDK.Persistance.Repositories;
+using Plugins.iBoxDB;
 
 namespace Tests
 {
@@ -24,22 +28,26 @@ namespace Tests
         [Test]
         public void TestMigrationOnEmptyRequestRepo()
         {
-            PlayerPrefs.DeleteAll();
             CountlyConfiguration configuration = new CountlyConfiguration {
                 ServerUrl = _serverUrl,
                 AppKey = _appKey,
             };
 
-            Assert.AreEqual(0, PlayerPrefs.GetInt(Constants.SchemaVersion));
-
             Countly.Instance.Init(configuration);
             Countly.Instance.RequestHelper._requestRepo.Clear();
 
             Assert.AreEqual(2, Countly.Instance.StorageHelper.SchemaVersion);
+
+            Countly.Instance.StorageHelper.CurrentVersion = 1;
+            Countly.Instance.StorageHelper.RunMigration();
+
+            int schemaVersion = PlayerPrefs.GetInt(Constants.SchemaVersion);
+            Assert.AreEqual(2, schemaVersion);
             Assert.AreEqual(2, Countly.Instance.StorageHelper.CurrentVersion);
             Assert.AreEqual(Countly.Instance.StorageHelper.SchemaVersion, Countly.Instance.StorageHelper.CurrentVersion);
-            Assert.AreEqual(2, PlayerPrefs.GetInt(Constants.SchemaVersion));
+
             Assert.AreEqual(0, Countly.Instance.RequestHelper._requestRepo.Count);
+
         }
 
         /// <summary>
@@ -51,19 +59,21 @@ namespace Tests
             CountlyConfiguration configuration = new CountlyConfiguration {
                 ServerUrl = _serverUrl,
                 AppKey = _appKey,
+                EnableConsoleLogging = false
             };
 
-            Countly.Instance.Init(configuration);
-            Countly.Instance.RequestHelper._requestRepo.Clear();
+            TempStorageHelper storageHelper = new TempStorageHelper(new CountlyLogHelper(configuration));
+            storageHelper.OpenDB();
+            storageHelper.ClearDBData();
 
             string url = "https://xyz.com/i?app_key=772c091355076ead703f987fee94490&device_id=57049b51faf44804a10967f54d8f8420&sdk_name=csharp-unity-editor&sdk_version=20.11.5&timestamp=1633595280409&hour=13&dow=4&tz=300&consent=%7b%0a++%22crashes%22%3a+true%2c%0a++%22events%22%3a+true%2c%0a++%22clicks%22%3a+true%2c%0a++%22star-rating%22%3a+true%2c%0a++%22views%22%3a+true%2c%0a++%22users%22%3a+true%2c%0a++%22sessions%22%3a+true%2c%0a++%22push%22%3a+true%2c%0a++%22remote-config%22%3a+true%2c%0a++%22location%22%3a+true%2c%0a++%22feedback%22%3a+true%0a%7d&checksum256=a3c63ddd0fa788eb05c75752533fdb8083960c4c35fb0ed5a689b631d2beb194";
             CountlyRequestModel request = new CountlyRequestModel(url, null);
-            Countly.Instance.RequestHelper.AddRequestToQueue(request);
-            Countly.Instance.StorageHelper.CurrentVersion = 1;
-            Countly.Instance.StorageHelper.RunMigration();
+            storageHelper.AddRequestToQueue(request);
+            storageHelper.CloseDB();
+            PlayerPrefs.DeleteAll();
 
-            Assert.AreEqual(1, Countly.Instance.RequestHelper._requestRepo.Count);
-
+            FirstLaunchAppHelper.Process();
+            Countly.Instance.Init(configuration);
             CountlyRequestModel requestModel = Countly.Instance.CrashReports._requestCountlyHelper._requestRepo.Dequeue();
 
             NameValueCollection collection = HttpUtility.ParseQueryString(requestModel.RequestData);
@@ -94,25 +104,28 @@ namespace Tests
                 AppKey = _appKey,
             };
 
-            Countly.Instance.Init(configuration);
-            Countly.Instance.RequestHelper._requestRepo.Clear();
+            TempStorageHelper storageHelper = new TempStorageHelper(new CountlyLogHelper(configuration));
+            storageHelper.OpenDB();
+            storageHelper.ClearDBData();
 
             string url = "https://xyz.com/i?app_key=772c091355076ead703f987fee94490&device_id=57049b51faf44804a10967f54d8f8420&sdk_name=csharp-unity-editor&sdk_version=20.11.5&timestamp=1633595280409&hour=13&dow=4&tz=300&consent=%7b%0a++%22crashes%22%3a+true%2c%0a++%22events%22%3a+true%2c%0a++%22clicks%22%3a+true%2c%0a++%22star-rating%22%3a+true%2c%0a++%22views%22%3a+true%2c%0a++%22users%22%3a+true%2c%0a++%22sessions%22%3a+true%2c%0a++%22push%22%3a+true%2c%0a++%22remote-config%22%3a+true%2c%0a++%22location%22%3a+true%2c%0a++%22feedback%22%3a+true%0a%7d&checksum256=a3c63ddd0fa788eb05c75752533fdb8083960c4c35fb0ed5a689b631d2beb194";
             CountlyRequestModel request = new CountlyRequestModel(url, null);
-            Countly.Instance.RequestHelper.AddRequestToQueue(request);
+            storageHelper.AddRequestToQueue(request);
 
-            url = "https://xyz.com/i?app_key=772c091355076ead703f987fee94490&device_id=57049b51faf44804a10967f54d8f8420&sdk_name=csharp-unity-editor&sdk_version=20.11.4&timestamp=1633595280409&hour=13&dow=4&tz=300&consent=%7b%0a++%22crashes%22%3a+true%2c%0a++%22events%22%3a+true%2c%0a++%22clicks%22%3a+true%2c%0a++%22star-rating%22%3a+true%2c%0a++%22views%22%3a+true%2c%0a++%22users%22%3a+true%2c%0a++%22sessions%22%3a+true%2c%0a++%22push%22%3a+true%2c%0a++%22remote-config%22%3a+true%2c%0a++%22location%22%3a+true%2c%0a++%22feedback%22%3a+true%0a%7d";
+            url = "https://xyz.com/i?app_key=772c091355076ead703f987fee94490&device_id=57049b51faf44804a10967f54d8f8420&sdk_name=csharp-unity-editor&sdk_version=20.11.5&timestamp=1633595280409&hour=13&dow=4&tz=300&consent=%7b%0a++%22crashes%22%3a+true%2c%0a++%22events%22%3a+true%2c%0a++%22clicks%22%3a+true%2c%0a++%22star-rating%22%3a+true%2c%0a++%22views%22%3a+true%2c%0a++%22users%22%3a+true%2c%0a++%22sessions%22%3a+true%2c%0a++%22push%22%3a+true%2c%0a++%22remote-config%22%3a+true%2c%0a++%22location%22%3a+true%2c%0a++%22feedback%22%3a+true%0a%7d&checksum256=a3c63ddd0fa788eb05c75752533fdb8083960c4c35fb0ed5a689b631d2beb194";
             request = new CountlyRequestModel(url, null);
-            Countly.Instance.RequestHelper.AddRequestToQueue(request);
+            storageHelper.AddRequestToQueue(request);
 
-            url = "https://xyz.com/i?app_key=772c091355076ead703f987fee94490&device_id=57049b51faf44804a10967f54d8f8420&sdk_name=csharp-unity-editor&sdk_version=20.11.3&timestamp=1633595280409&hour=13&dow=4&tz=300&consent=%7b%0a++%22crashes%22%3a+true%2c%0a++%22events%22%3a+true%2c%0a++%22clicks%22%3a+true%2c%0a++%22star-rating%22%3a+true%2c%0a++%22views%22%3a+true%2c%0a++%22users%22%3a+true%2c%0a++%22sessions%22%3a+true%2c%0a++%22push%22%3a+true%2c%0a++%22remote-config%22%3a+true%2c%0a++%22location%22%3a+true%2c%0a++%22feedback%22%3a+true%0a%7d&checksum256=a3c63ddd0fa788eb05c75752533fdb8083960c4c35fb0ed5a689b631d2beb194";
+            url = "https://xyz.com/i?app_key=772c091355076ead703f987fee94490&device_id=57049b51faf44804a10967f54d8f8420&sdk_name=csharp-unity-editor&sdk_version=20.11.5&timestamp=1633595280409&hour=13&dow=4&tz=300&consent=%7b%0a++%22crashes%22%3a+true%2c%0a++%22events%22%3a+true%2c%0a++%22clicks%22%3a+true%2c%0a++%22star-rating%22%3a+true%2c%0a++%22views%22%3a+true%2c%0a++%22users%22%3a+true%2c%0a++%22sessions%22%3a+true%2c%0a++%22push%22%3a+true%2c%0a++%22remote-config%22%3a+true%2c%0a++%22location%22%3a+true%2c%0a++%22feedback%22%3a+true%0a%7d&checksum256=a3c63ddd0fa788eb05c75752533fdb8083960c4c35fb0ed5a689b631d2beb194";
             request = new CountlyRequestModel(url, null);
-            Countly.Instance.RequestHelper.AddRequestToQueue(request);
+            storageHelper.AddRequestToQueue(request);
 
-            Countly.Instance.StorageHelper.CurrentVersion = 1;
-            Countly.Instance.StorageHelper.RunMigration();
+            storageHelper.CloseDB();
+            PlayerPrefs.DeleteAll();
 
-            Assert.AreEqual(3, Countly.Instance.RequestHelper._requestRepo.Count);
+            FirstLaunchAppHelper.Process();
+
+            Countly.Instance.Init(configuration);
 
             int schemaVersion = PlayerPrefs.GetInt(Constants.SchemaVersion);
             Assert.AreEqual(2, schemaVersion);
@@ -391,6 +404,66 @@ namespace Tests
         {
             Countly.Instance.ClearStorage();
             Object.DestroyImmediate(Countly.Instance);
+        }
+
+        private class TempStorageHelper {
+
+            private DB _db;
+            private CountlyLogHelper _logHelper;
+            internal RequestRepository RequestRepo { get; private set; }
+            internal Dao<RequestEntity> RequestDao { get; private set; }
+
+            internal TempStorageHelper(CountlyLogHelper logHelper)
+            {
+                _logHelper = logHelper;
+            }
+
+            /// <summary>
+            /// Create database and tables
+            /// </summary>
+            private DB BuildDatabase(long dbNumber)
+            {
+                DB.Root(Application.persistentDataPath);
+                DB db = new DB(dbNumber);
+
+                db.GetConfig().EnsureTable<SegmentEntity>(EntityType.Configs.ToString(), "Id");
+                db.GetConfig().EnsureTable<RequestEntity>(EntityType.Requests.ToString(), "Id");
+                db.GetConfig().EnsureTable<EventEntity>(EntityType.NonViewEvents.ToString(), "Id");
+                db.GetConfig().EnsureTable<SegmentEntity>(EntityType.NonViewEventSegments.ToString(), "Id");
+
+                return db;
+            }
+
+            /// <summary>
+            /// Open database connection and initialize data access objects.
+            /// </summary>
+            internal void OpenDB()
+            {
+                _db = BuildDatabase(3);
+                DB.AutoBox auto = _db.Open();
+
+                RequestDao = new Dao<RequestEntity>(auto, EntityType.Requests.ToString(), _logHelper);
+                RequestRepo = new RequestRepository(RequestDao, _logHelper);
+                RequestRepo.Initialize();
+            }
+
+            internal void AddRequestToQueue(CountlyRequestModel request)
+            {
+                RequestRepo.Enqueue(request);
+            }
+
+            /// <summary>
+            /// Close database connection.
+            /// </summary>
+            internal void CloseDB()
+            {
+                _db.Close();
+            }
+
+            internal void ClearDBData()
+            {
+                RequestRepo.Clear();
+            }
         }
     }
 }
