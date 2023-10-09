4467739d5a625ad3d8089d743987623229d54c16
alirizakat <ali.kat@count.ly>
2023-10-09 15:09:01 +0300

    Renaming

--- Assets/Plugins/Editor/CountlyEditorMenu.cs
+++ Assets/Plugins/Editor/CountlyEditorMenu.cs
@@ -3,8 +3,8 @@ using UnityEditor;
 
 public class CountlyEditorMenu
 {
-    [MenuItem("Countly/SDK Document")]
-    private static void SDKDocument()
+    [MenuItem("Countly/SDK Documentation")]
+    private static void SDKDocumentation()
     {
         Application.OpenURL("https://support.count.ly/hc/en-us/articles/360037813851-Unity");
     }
