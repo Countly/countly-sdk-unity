fff9c1b48811e46db04b677f3caa20160dfff2e4
alirizakat <ali.kat@count.ly>
2023-10-24 12:05:10 +0300

    constants updated

--- Assets/Plugins/CountlySDK/Helpers/Constants.cs
+++ Assets/Plugins/CountlySDK/Helpers/Constants.cs
@@ -5,7 +5,7 @@ namespace Plugins.CountlySDK.Helpers
 {
     internal class Constants
     {
-        public const string SdkVersion = "23.06.0";
+        public const string SdkVersion = "23.06.1";
 
 #if UNITY_EDITOR
         public const string SdkName = "csharp-unity-editor";
