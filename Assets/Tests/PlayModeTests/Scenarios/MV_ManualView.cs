bdc98fefbb6a77598e97cd2865587c3762c5e390
Ali Rıza Kat <ali.kat@count.ly>
2024-02-08 14:44:48 +0300

    Codacy fix

--- Assets/Tests/PlayModeTests/Scenarios/MV_ManualView.cs
+++ Assets/Tests/PlayModeTests/Scenarios/MV_ManualView.cs
@@ -7,13 +7,12 @@ using Plugins.CountlySDK;
 using Plugins.CountlySDK.Enums;
 using Plugins.CountlySDK.Models;
 using Plugins.CountlySDK.Services;
-using UnityEngine;
 
 namespace Assets.Tests.PlayModeTests.Scenarios
 {
     public class CustomIdProvider : ISafeIDGenerator
     {
-        static int viewCount = 0;
+        int viewCount;
 
         public string GenerateValue()
         {
@@ -29,26 +28,26 @@ namespace Assets.Tests.PlayModeTests.Scenarios
         ViewCountlyService viewService;
         readonly ISafeIDGenerator idGenerator = new CustomIdProvider();
 
-        Action<ViewCountlyService, string, Dictionary<string, object>>[] allMethods = new Action<ViewCountlyService, string, Dictionary<string, object>>[]
+        readonly Action<ViewCountlyService, string, Dictionary<string, object>>[] allMethods = new Action<ViewCountlyService, string, Dictionary<string, object>>[]
          {
-            (service, arg1, arg2) => service.RecordOpenViewAsync(arg1), // 0
+            (service, arg1, _) => service.RecordOpenViewAsync(arg1), // 0
             (service, arg1, arg2) => service.RecordOpenViewAsync(arg1, arg2), // 1
-            (service, arg1, arg2) => service.RecordCloseViewAsync(arg1), // 2
-            (service, arg1, arg2) => service.StartView(arg1), // 3
+            (service, arg1, _) => service.RecordCloseViewAsync(arg1), // 2
+            (service, arg1, _) => service.StartView(arg1), // 3
             (service, arg1, arg2) => service.StartView(arg1, arg2), // 4
-            (service, arg1, arg2) => service.StartAutoStoppedView(arg1), // 5
+            (service, arg1, _) => service.StartAutoStoppedView(arg1), // 5
             (service, arg1, arg2) => service.StartAutoStoppedView(arg1, arg2), // 6
-            (service, arg1, arg2) => service.StopViewWithName(arg1), // 7
+            (service, arg1, _) => service.StopViewWithName(arg1), // 7
             (service, arg1, arg2) => service.StopViewWithName(arg1, arg2), // 8
-            (service, arg1, arg2) => service.StopViewWithID(arg1), // 9
+            (service, arg1, _) => service.StopViewWithID(arg1), // 9
             (service, arg1, arg2) => service.StopViewWithID(arg1, arg2), // 10
-            (service, arg1, arg2) => service.PauseViewWithID(arg1), // 11
-            (service, arg1, arg2) => service.ResumeViewWithID(arg1), // 12
-            (service, arg1, arg2) => service.StopAllViews(arg2), // 13
-            (service, arg1, arg2) => service.SetGlobalViewSegmentation(arg2), // 14
+            (service, arg1, _) => service.PauseViewWithID(arg1), // 11
+            (service, arg1, _) => service.ResumeViewWithID(arg1), // 12
+            (service, _, arg2) => service.StopAllViews(arg2), // 13
+            (service, _, arg2) => service.SetGlobalViewSegmentation(arg2), // 14
             (service, arg1, arg2) => service.AddSegmentationToViewWithID(arg1, arg2), // 15
             (service, arg1, arg2) => service.AddSegmentationToViewWithName(arg1, arg2), // 16
-            (service, arg1, arg2) => service.UpdateGlobalViewSegmentation(arg2), // 17
+            (service, _, arg2) => service.UpdateGlobalViewSegmentation(arg2), // 17
          };
 
         Dictionary<string, object> testSegmentation;
@@ -88,7 +87,6 @@ namespace Assets.Tests.PlayModeTests.Scenarios
                 func(viewService, null, testSegmentation);
                 Assert.IsTrue(Countly.Instance.RequestHelper._requestRepo.Count == 0); // validate that RQ is empty
                 Assert.IsTrue(Countly.Instance.Views._eventService._eventRepo.Count == 0); // validate that EQ is empty
-                Debug.Log($"{func.Method.Name} executed successfully with null values.");
             }
         }
 
@@ -105,7 +103,6 @@ namespace Assets.Tests.PlayModeTests.Scenarios
                 func(viewService, "", testSegmentation);
                 Assert.IsTrue(Countly.Instance.RequestHelper._requestRepo.Count == 0); // validate that RQ is empty
                 Assert.IsTrue(Countly.Instance.Views._eventService._eventRepo.Count == 0); // validate that EQ is empty
-                Debug.Log($"{func.Method.Name} executed successfully with empty string values.");
             }
         }
 
@@ -129,7 +126,6 @@ namespace Assets.Tests.PlayModeTests.Scenarios
 
                 Assert.IsTrue(Countly.Instance.RequestHelper._requestRepo.Count == 0); // validate that RQ is empty
                 Assert.IsTrue(Countly.Instance.Views._eventService._eventRepo.Count == 0); // validate that EQ is empty
-                Debug.Log($"{func.Method.Name} executed successfully with empty string values.");
             }
         }
 
@@ -146,16 +142,12 @@ namespace Assets.Tests.PlayModeTests.Scenarios
             viewService.StartAutoStoppedView(viewA);
             Thread.Sleep(1000);
 
-            // eE_A d=1 id=idv1 pvid="", segm={}
             CountlyEventModel viewEventAStart = viewService._eventService._eventRepo.Dequeue();
             Assert.AreEqual(viewEventAStart.EventID, "idv1");
             Assert.AreEqual(viewEventAStart.PreviousViewID, "");
             Assert.AreEqual(viewEventAStart.Segmentation["visit"], 1);
             Assert.AreEqual(viewEventAStart.Segmentation["start"], 1);
 
-            Debug.Log(viewEventAStart);
-
-            // eE_A d=1 id=idv1 pvid="", segm={}, sE_B id=idv2 pvid=idv1 segm={visit="1"}
             viewService.StartAutoStoppedView(viewB);
             CountlyEventModel viewEventAEnd = viewService._eventService._eventRepo.Dequeue();
 
