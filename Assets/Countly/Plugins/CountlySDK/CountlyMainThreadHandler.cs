35c297d61b222959aba7c636e63ed5c027ab0603
Ali Rıza Kat <ali.kat@count.ly>
2024-02-22 11:06:18 +0300

    Added some comments in MainThreadDispatcher

--- Assets/Countly/Plugins/CountlySDK/CountlyMainThreadHandler.cs
+++ Assets/Countly/Plugins/CountlySDK/CountlyMainThreadHandler.cs
@@ -14,16 +14,28 @@ public class CountlyMainThreadHandler : MonoBehaviour
     {
         get {
             if (_instance == null) {
+                // If instance is null, add this script to the created Countly object
                 GameObject gameObject = Countly.Instance.gameObject;
                 _instance = gameObject.AddComponent<CountlyMainThreadHandler>();
             }
             return _instance;
         }
         internal set {
+            // Allow internal setting of the instance (used during cleanup)
             _instance = value;
         }
     }
 
+    private void Awake()
+    {
+        // Record the main thread when the script is first initialized
+        mainThread = Thread.CurrentThread;
+    }
+
+    /// <summary>
+    /// Checks if the current thread is the main thread
+    /// </summary>
+    /// <returns></returns>
     public bool IsMainThread()
     {
         return Thread.CurrentThread.ManagedThreadId == mainThread.ManagedThreadId;
@@ -33,23 +45,19 @@ public class CountlyMainThreadHandler : MonoBehaviour
     {
         // Check if we are on the main thread
         if (IsMainThread()) {
+            // If on the main thread, invoke the action immediately
             action.Invoke();
         } else {
-            // Queue the action to be executed on the main thread
+            // If on a different thread, queue the action to be executed on the main thread
             lock (lockObject) {
                 _queuedAction = action;
             }
         }
     }
 
-    private void Awake()
-    {
-        mainThread = Thread.CurrentThread;
-    }
-
     private void Update()
     {
-        // Execute any queued action on the main thread
+        // Execute any queued action on the main thread during the Unity Update phase
         if (_queuedAction != null) {
             lock (lockObject) {
                 _queuedAction.Invoke();
