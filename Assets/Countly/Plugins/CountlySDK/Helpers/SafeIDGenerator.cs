be3eda251e75a963ed920a6ba886a5f3d55d73e6
Ali Rıza Kat <ali.kat@count.ly>
2024-04-02 15:00:16 +0300

    Update SafeIDGenerator.cs

--- Assets/Countly/Plugins/CountlySDK/Helpers/SafeIDGenerator.cs
+++ Assets/Countly/Plugins/CountlySDK/Helpers/SafeIDGenerator.cs
@@ -1,9 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Plugins.CountlySDK
 {
     public interface ISafeIDGenerator
     {
         string GenerateValue();
     }
+
     public class SafeIDGenerator : ISafeIDGenerator
     {
         public string GenerateValue()
