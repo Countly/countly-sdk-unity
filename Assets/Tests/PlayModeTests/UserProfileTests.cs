d7b2ef94ed050da9cca015b0db43e14b4ac98d6e
Ali Rıza Kat <ali.kat@count.ly>
2024-07-18 17:13:47 +0300

    Codacy Update + New Tests

--- Assets/Tests/PlayModeTests/UserProfileTests.cs
+++ Assets/Tests/PlayModeTests/UserProfileTests.cs
@@ -35,10 +35,10 @@ namespace Assets.Tests.PlayModeTests
                 { "Integer", 26 },
                 { "Float", 3.1f },
                 { "String", "something special cooking" },
-                { "IntArray", new int[] { 1, 2, 3 } },
-                { "BoolArray", new bool[] { true, false, true } },
-                { "FloatArray", new float[] { 1.1f, 2.2f, 3.3f } },
-                { "StringArray", new string[] { "a", "b", "c" } },
+                { "IntArray", new [] { 1, 2, 3 } },
+                { "BoolArray", new [] { true, false, true } },
+                { "FloatArray", new [] { 1.1f, 2.2f, 3.3f } },
+                { "StringArray", new [] { "a", "b", "c" } },
                 { "IntList", new List<int> { 1, 2, 3 } },
                 { "BoolList", new List<bool> { true, false, true } },
                 { "FloatList", new List<float> { 1.1f, 2.2f, 3.3f } },
@@ -46,6 +46,39 @@ namespace Assets.Tests.PlayModeTests
             };
 
             return expectedUserDetails;
+        }
+
+        private Dictionary<string, object> ExpectedUserData()
+        {
+            var expectedUserDetails = new Dictionary<string, object>
+            {
+                { "a12345", "My Property" },
+                { "b12345", new Dictionary<string, object> { { "$inc", 1.0 } } },
+                { "c12345", new Dictionary<string, object> { { "$inc", 10.0 } } },
+                { "d12345", new Dictionary<string, object> { { "$mul", 20.0 } } },
+                { "e12345", new Dictionary<string, object> { { "$max", 100.0 } } },
+                { "f12345", new Dictionary<string, object> { { "$min", 50.0 } } },
+                { "g12345", new Dictionary<string, object> { { "$setOnce", "200" } } },
+                { "h12345", new Dictionary<string, object> { { "$addToSet", new List<string> { "morning" } } } },
+                { "i12345", new Dictionary<string, object> { { "$push", new List<string> { "morning" } } } },
+                { "k12345", new Dictionary<string, object> { { "$pull", new List<string> { "morning" } } } }
+            };
+
+            return expectedUserDetails;
+        }
+
+        private void SendUserData()
+        {
+            Countly.Instance.UserProfile.SetProperty("a12345", "My Property");
+            Countly.Instance.UserProfile.Increment("b12345");
+            Countly.Instance.UserProfile.IncrementBy("c12345", 10);
+            Countly.Instance.UserProfile.Multiply("d12345", 20);
+            Countly.Instance.UserProfile.Max("e12345", 100);
+            Countly.Instance.UserProfile.Min("f12345", 50);
+            Countly.Instance.UserProfile.SetOnce("g12345", "200");
+            Countly.Instance.UserProfile.PushUnique("h12345", "morning");
+            Countly.Instance.UserProfile.Push("i12345", "morning");
+            Countly.Instance.UserProfile.Pull("k12345", "morning");
         }
 
         // 'SetUserProperties' in CountlyConfiguration
@@ -150,9 +183,52 @@ namespace Assets.Tests.PlayModeTests
             cly.UserProfile.Save();
 
             TestUtility.ValidateRQEQSize(cly, 2, 0);
-            Dictionary<string, object> up3 = TestUtility.ExtractAndDeserializeUserDetails(cly.RequestHelper._requestRepo.Models);
-            TestUtility.ValidateUserDetails(up3, ExpectedUserProperty());
+            Dictionary<string, object> up1 = TestUtility.ExtractAndDeserializeUserDetails(cly.RequestHelper._requestRepo.Models);
+            TestUtility.ValidateUserDetails(up1, ExpectedUserProperty());
             cly.RequestHelper._requestRepo.Clear();
+        }
+        
+        // 'Save' in Countly.Instance.UserProfile
+        // We initialize the sdk with consent requirement however not give consent. We try to record user profile data and call save
+        // Since consent is not provided calling 'Save' should not record any request
+        [Test]
+        public void SaveWithRecordedValue_CR_CNG()
+        { 
+            CountlyConfiguration config = TestUtility.CreateBaseConfig()
+                .SetRequiresConsent(true);
+
+            Countly cly = Countly.Instance;
+            cly.Init(config);
+
+            SendUserData();
+            cly.UserProfile.Save();
+
+            // 1 request for session and 1 for consents
+            TestUtility.ValidateRQEQSize(cly, 2, 0);
+            // which should return an empty dictionary since there are no user profile requests
+            Dictionary<string, object> up1 = TestUtility.ExtractAndDeserializeUserDetails(cly.RequestHelper._requestRepo.Models);
+            Assert.IsTrue(up1.Count == 0);
+        }
+        
+        // 'Save' in Countly.Instance.UserProfile
+        // We initialize the sdk with consent requirement and give consent. We try to record user profile data and call save
+        // Since consent is provided calling 'Save' should record an User Profile request
+        [Test]
+        public void SaveWithRecordedValue_CR_CG()
+        { 
+            Consents[] consent = new Consents[] { Consents.Crashes, Consents.Events, Consents.Clicks, Consents.StarRating, Consents.Views, Consents.Users, Consents.Push, Consents.RemoteConfig, Consents.Location, Consents.Feedback, Consents.Sessions };
+            CountlyConfiguration config = TestUtility.CreateBaseConfigConsent(consent);
+
+            Countly cly = Countly.Instance;
+            cly.Init(config);
+
+            SendUserData();
+            cly.UserProfile.Save();
+
+            TestUtility.ValidateRQEQSize(cly, 3, 0);
+            // which should return an empty dictionary since there are no user profile requests
+            Dictionary<string, object> up1 = TestUtility.ExtractAndDeserializeUserDetails(cly.RequestHelper._requestRepo.Models);
+            TestUtility.ValidateUserDetails(up1, ExpectedUserData());
         }
 
         [SetUp]
