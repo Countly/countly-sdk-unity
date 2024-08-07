410f47aa864766ac1ae69647b312b14367af7947
Ali Rıza Kat <ali.kat@count.ly>
2024-08-07 15:44:09 +0300

    Added UserProfile Calls & Edited Legacy Calls

--- Assets/Countly/Example/Scripts/UserDetail.cs
+++ Assets/Countly/Example/Scripts/UserDetail.cs
@@ -11,6 +11,90 @@ using UnityEngine.SceneManagement;
 
 public class UserDetail : MonoBehaviour
 {
+    public void Increment()
+    {
+        Countly.Instance.UserProfile.Increment("Samurai");
+    }
+
+    public void IncrementBy()
+    {
+        Countly.Instance.UserProfile.IncrementBy("Silverhand", 2077);
+    }
+
+    public void SaveMax()
+    {
+        Countly.Instance.UserProfile.SaveMax("PowerLevel", 9001);
+    }
+
+    public void SaveMin()
+    {
+        Countly.Instance.UserProfile.SaveMin("BestTime", 23);
+    }
+
+    public void Multiply()
+    {
+        Countly.Instance.UserProfile.Multiply("CurrentScore", 2);
+    }
+
+    public void Pull()
+    {
+        Countly.Instance.UserProfile.Pull("Inventory", "Blackblade");
+    }
+
+    public void Push()
+    {
+        Countly.Instance.UserProfile.Push("Backpack", "Unity");
+    }
+
+    public void PushUnique()
+    {
+        Countly.Instance.UserProfile.PushUnique("OneTimeItems", "AccessCard");
+    }
+
+    public void Save()
+    {
+        Countly.Instance.UserProfile.Save();
+    }
+
+    public void SetOnce()
+    {
+        Countly.Instance.UserProfile.SetOnce("ChosenEnding", "Ending1");
+    }
+
+    public void SetProperties()
+    {
+        Dictionary<string, object> userProperties = new Dictionary<string, object> {
+            { "name", "Johnny Silverhand" },
+            { "username", "silverhand" },
+            { "email", "info@samurai.com" },
+            { "organization", "Samurai" },
+            { "phone", "+1 555 123 4567" },
+            { "picture", "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/01/cyberpunk-2077-johnny-silverhand-glasses.jpg" },
+            { "gender", "M" },
+            { "byear", 1988 },
+            { "Boolean", true },
+            { "Integer", 34 },
+            { "Float", 6.2f },
+            { "Quote", "Wake up, Samurai. We have a city to burn." },
+            { "SignificantYears", new [] { 2020, 2077, 2088 } },
+            { "TruthValues", new [] { true, false, true } },
+            { "SkillLevels", new [] { 1.5f, 2.5f, 3.5f } },
+            { "FavoriteItems", new [] { "guitar", "revolver", "cyberarm" } },
+            { "YearList", new List<int> { 2020, 2077, 2088 } },
+            { "BooleanList", new List<bool> { true, false, true } },
+            { "SkillLevelList", new List<float> { 1.5f, 2.5f, 3.5f } },
+            { "FavoriteItemList", new List<string> { "guitar", "revolver", "cyberarm" } }
+        };
+
+        Countly.Instance.UserProfile.SetProperties(userProperties);
+    }
+
+    public void SetProperty()
+    {
+        Countly.Instance.UserProfile.SetProperty("Best Attribute", "Int");
+    }
+
+    #region LegacyCode
     public async void SetUserDetail()
     {
         CountlyUserDetailsModel userDetails = new CountlyUserDetailsModel(
@@ -53,13 +137,13 @@ public class UserDetail : MonoBehaviour
         await Countly.Instance.UserDetails.SaveAsync();
     }
 
-    public async void IncrementBy()
+    public async void IncrementBy_Legacy()
     {
         Countly.Instance.UserDetails.IncrementBy("ShoeSize", 2);
         await Countly.Instance.UserDetails.SaveAsync();
     }
 
-    public async void Multiply()
+    public async void Multiply_Legacy()
     {
         Countly.Instance.UserDetails.Multiply("PetNumber", 2);
         await Countly.Instance.UserDetails.SaveAsync();
@@ -77,19 +161,19 @@ public class UserDetail : MonoBehaviour
         await Countly.Instance.UserDetails.SaveAsync();
     }
 
-    public async void Push()
+    public async void Push_Legacy()
     {
         Countly.Instance.UserDetails.Push("Area", new string[] { "width", "height" });
         await Countly.Instance.UserDetails.SaveAsync();
     }
 
-    public async void PushUnique()
+    public async void PushUnique_Legacy()
     {
         Countly.Instance.UserDetails.PushUnique("Mole", new string[] { "Left Cheek", "Right Cheek" });
         await Countly.Instance.UserDetails.SaveAsync();
     }
 
-    public async void Pull()
+    public async void Pull_Legacy()
     {
         //Remove one or many values
         Countly.Instance.UserDetails.Pull("Cat", new string[] { "Claw" });
@@ -104,4 +188,10 @@ public class UserDetail : MonoBehaviour
         Countly.Instance.UserDetails.Push("Inventory", new string[] { "Sword", "Shield", "Armor" });
         await Countly.Instance.UserDetails.SaveAsync();
     }
+
+    public async void Save_Legacy()
+    {
+        await Countly.Instance.UserDetails.SaveAsync();
+    }
+    #endregion
 }
